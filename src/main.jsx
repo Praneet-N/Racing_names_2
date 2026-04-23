9280942e98fba51dd0c50d0d85148e14043b2857
Praneet Namakal <praneet.namakal@gmail.com>
2026-04-23 16:50:03 +1000

    Create src/main.jsx file

---
+++ src/main.jsx
@@ -0,0 +1,5 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import MeetingGP from './App'
+
+ReactDOM.createRoot(document.getElementById('root')).render(<React.StrictMode><MeetingGP /></React.StrictMode>)
